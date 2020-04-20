6334512a1ceff0c7fa416a6445ecc7eef732bb00
KarolK <xvilks@wp.pl>
2020-04-20 21:28:58 +0200

    Second task - fixed goldenSection method by changing algorithm steps (wikipedia's iterative algorithm)

--- src/firstTask.jl
+++ src/firstTask.jl
@@ -1,48 +1,28 @@
 module firstTask
 
 # Global variable
-p = (3 - sqrt(5)) / 2
+goldenRatio = (1 + sqrt(5)) / 2
 
 # Golden section method to find local minimum value
-function goldenSection(f, a, b, tol=1e-5)
+function goldenSection(f, a, b, tol = 1e-8)
   # Make sure that entered range is correct
   a = min(a, b)
   b = max(a, b)
-  # Algorithm starts there
-  k = 1
-  w1 = a + (1 - p) * (b - a)
-  w2 = a + p * (b - a)
-  yw1 = f(w1)
-  yw2 = f(w2)
 
-  while abs(b - a) >= tol
-    println(a, " <-> ", b, ": ", w1, ", ", w2)
-    if yw1 > yw2
-      # Minimum is located in right section - reassign proper values
-      a = w1
-      w1 = w2
-      yw1 = yw2
-      # Calculate other values
-      w2 = a + p * (b - a)
-      yw2 = f(w2)
-    elseif yw1 < yw2
-      # Minimum is located in left section - reassign proper values
-      b = w2
-      w2 = w1
-      yw2 = yw1
-      # Calculate other values
-      w1 = a + (1 - p) * (b - a)
-      yw1 = f(w1)
+  while abs(a - b) >= tol
+    # Calculate next 2 points
+    x1 = b - (b - a) / goldenRatio
+    x2 = a + (b - a) / goldenRatio
+    # Compare function values from calculated points and determine where minimum is located
+    if f(x1) < f(x2)
+      b = x2
     else
-      println("No minimum found...")
+      a = x1
     end
-    k += 1
   end
-
-  result = round((a + b) / 2)
-
-  println("Final range: (a, b) = (", a, ", ", b, ")")
-  println("Approximate result = ", result)
+  # Get the approximate value and return it
+  result = (a + b) / 2
+  return result
 end
 
 export goldenSection
