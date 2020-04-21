966d15f2bfa8cf575e1079f3b960b972433ea2a9
KarolK <xvilks@wp.pl>
2020-04-21 12:52:26 +0200

    Second task - added testset to goldSection method

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,15 @@
 using firstTask, Test
 
-@test my_f(1) == 2
-@test my_f(2) == 8
-@test my_f(3) == 18
+@testset "goldenSection" begin
+  f(x) = x^4 + 3x^3 + x^2 + sin(x)
+  a = -10
+  b = 10
+  tol = 1e-6
+  @test goldenSection(f, a, b, tol) == (-1.9712755845857126, -4.915216432826815)
+
+  f(x) = 3(x - 4)^2
+  a = 0
+  b = 10
+  tol = 1e-6
+  @test goldenSection(f, a, b, tol) == (4.000000004370129, 5.729408629607859e-17)
+end
